b7eba2716f590632f59ee79b562cf39fab024fb5
naimmustafa <naimmetinmustafa@gmail.com>
2024-06-11 22:12:23 +0100

    [IDO-Feature-2] Extra Photo Page Accordion, Extra gas page state handler update

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,94 +1,68 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import {
-  Alert,
-  SafeAreaView,
+  View,
   ScrollView,
   StyleSheet,
+  SafeAreaView,
   TouchableOpacity,
-  View,
 } from 'react-native';
-import { height, unitH } from '../utils/constant';
-import { PrimaryColors } from '../theme/colors';
-import { EcomPressable as Button } from '../components/ImageButton';
-import Text from '../components/Text';
 import { useNavigation } from '@react-navigation/native';
-import { AppContext } from '../context/AppContext';
+
+// Components
+import Text from '../components/Text';
+import { EcomPressable as Button } from '../components/ImageButton';
+
+// Context
+import { PrimaryColors } from '../theme/colors';
 import { useAuth } from '../context/AuthContext';
-import { getDatabaseJob } from '../utils/database';
+
+const buttons = [
+  {
+    title: 'Engineers Calendar',
+    navigationName: 'CalendarPage',
+  },
+  {
+    title: 'New Job',
+    navigationName: 'NewJobPage',
+  },
+  {
+    title: 'Planned Job',
+    navigationName: 'PlannedJobPage',
+  },
+  {
+    title: 'Jobs in progress',
+    navigationName: 'InProgressJobsPage',
+  },
+  {
+    title: 'Completed Job',
+    navigationName: 'CompletedJobsPage',
+  },
+];
 
 function HomePage() {
   const navigation = useNavigation();
 
   const { OnLogout } = useAuth();
-  const appContext = useContext(AppContext);
+
   const navigationToPage = ({ navigationName }) => {
     navigation.navigate(navigationName);
   };
-  const checkJobsAndNavigate = async () => {
-    try {
-      const jobs = (await getDatabaseJob()) || []; // Ensure an array is always returned
-      const inProgressJobs = jobs.filter((job) => job.status === 'In Progress');
 
-      if (inProgressJobs.length >= 6) {
-        Alert.alert(
-          'Limit Reached',
-          'Please finish or delete existing jobs to continue.'
-        );
-      } else {
-        navigationToPage({ navigationName: 'NewJobPage' });
-      }
-    } catch (error) {
-      console.error('Failed to check job status', error);
-      Alert.alert('Error', 'Failed to fetch job data.'); // Now Alert should work
-    }
-  };
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ScrollView>
         <View style={styles.body}>
-          <Button
-            onPress={() => {
-              navigationToPage({ navigationName: 'CalendarPage' });
-            }}
-            style={styles.button}
-          >
-            <Text style={styles.buttonTxt}>Engineers Calendar</Text>
-          </Button>
-          <Button
-            onPress={() => {
-              navigationToPage({ navigationName: 'NewJobPage' });
-            }}
-            style={styles.button}
-          >
-            <Text style={styles.buttonTxt}>New Job</Text>
-          </Button>
-          <Button
-            onPress={() => {
-              navigationToPage({ navigationName: 'PlannedJobPage' });
-            }}
-            style={styles.button}
-          >
-            <Text style={styles.buttonTxt}>Planned Job</Text>
-          </Button>
-
-          <Button
-            onPress={() => {
-              navigationToPage({ navigationName: 'InProgressJobsPage' });
-            }}
-            style={styles.button}
-          >
-            <Text style={styles.buttonTxt}>Jobs in progress</Text>
-          </Button>
-          <Button
-            onPress={() => {
-              navigationToPage({ navigationName: 'CompletedJobsPage' });
-            }}
-            style={styles.button}
-          >
-            <Text style={styles.buttonTxt}>Completed Job</Text>
-          </Button>
-          
-
+          {buttons.map((button) => (
+            <Button
+              onPress={() => {
+                navigationToPage({ navigationName: button.navigationName });
+              }}
+              style={styles.button}
+              key={button.navigationName}
+            >
+              <Text style={styles.buttonTxt}>{button.title}</Text>
+            </Button>
+          ))}
           <TouchableOpacity
             onPress={OnLogout}
             style={{
@@ -111,24 +85,18 @@ const styles = StyleSheet.create({
     padding: 20,
     gap: 20,
   },
-  spacer: {
-    height: unitH * 30,
-  },
   button: {
-    height: unitH * 150,
+    height: 100,
     backgroundColor: PrimaryColors.White,
     alignItems: 'center',
     justifyContent: 'center',
-    borderWidth: 1,
     borderColor: PrimaryColors.Black,
     elevation: 5,
-    shadowColor: '#000',
-    shadowOpacity: 0.5,
-    shadowRadius: 1,
-    shadowOffset: {
-      width: 2,
-      height: 2,
-    },
+    borderRadius: 5,
+    shadowColor: PrimaryColors.Black,
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
   },
   buttonTxt: {
     color: 'black',
